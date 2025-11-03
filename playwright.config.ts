4e1c38501a549096fb00dddee127fdffbbdeed82
Your Name <shiqi90@gmail.com>
2025-11-03 09:41:11 +0000

    Report in file format

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+//////  reporter: 'html',
+ reporter: [
+    ['list'], // Console output
+    ['html', { outputFolder: 'my-html-report', open: 'never' }], // HTML report
+    ['json', { outputFile: 'results.json' }], // JSON report
+    ['junit', { outputFile: 'results.xml' }], // JUnit XML report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
