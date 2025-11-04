ffce70a3befd9073c0f6412096a82cd83f6d3a97
xetter9 <you@example.com>
2025-11-04 09:31:43 +0000

    test results of home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,7 +4,14 @@ test('Home page', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/');
   
   //Ensure the sign-in link is present
-    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+  await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
 
+  // Check the title of the page
   await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");  
+
+  // Check the count of items displayed
+  const productGrid = page.locator(".col-md-9");
+  await expect(productGrid.getByRole("link")).toHaveCount(9);
+  expect(await productGrid.getByRole("link").count()).toBe(9);
+
 });
