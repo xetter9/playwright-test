1b611935b8f04c679e2483d878f3d9584e828420
xetter9 <you@example.com>
2025-11-04 09:22:01 +0000

    Result

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,7 +4,7 @@ test('Home page', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/');
   
   //Ensure the sign-in link is present
-//  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign In");
-    await expect(page.getByTestId('nav-sign-in')).toBeVisible();
-  
+    await expect(page.getByTestId('nav-sign-in')).toHaveText('Sign in');
+
+  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");  
 });
